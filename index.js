953f30eec5e0deb2f01a177e5a6b2083b6169b52
ashirbad <ashirbadbehera29@gmail.com>
2021-06-03 16:29:57 +0530

    ✨add quote feature

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+// const getQuote = require('./apiCalls.js');
+const fetch = require('node-fetch');
+
 const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 dotenv.config();
+
+const getQuote = () => {
+	// @ts-ignore
+	return fetch('https://zenquotes.io/api/random')
+		.then(res => res.json())
+		.then(data => {
+			return `>>> *${data[0].q} - ${data[0].a}*`;
+		});
+};
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -11,7 +23,18 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (msg.content === `${prefix}hello`) {
-		msg.reply('Yo, Hope your doing fine!');
+		msg.reply('Yo, Hope you are doing fine!');
+	} else if (msg.content === `${prefix}clear`) {
+		// console.log('hello');
+		msg.channel.messages.fetch().then(messages => {
+			messages.forEach(m => {
+				m.delete();
+			});
+		});
+	} else if (msg.content === `${prefix}inspire`) {
+		console.log('called');
+		// @ts-ignore
+		getQuote().then(quote => msg.channel.send(quote));
 	}
 });
 
