fd87e6f18e0f9491af1abc9ce7888638fdb40c4e
ashirbad <ashirbadbehera29@gmail.com>
2021-06-03 12:19:58 +0530

    ➕add configs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 dotenv.config();
@@ -9,8 +10,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('Pong!');
+	if (msg.content === `${prefix}hello`) {
+		msg.reply('Yo, Hope your doing fine!');
 	}
 });
 
